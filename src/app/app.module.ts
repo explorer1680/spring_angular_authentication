0300d5851ba85266c769f6e653676c10d950c042
echeng <echeng@ci.com>
2019-01-09 14:54:09 -0500

    step 2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AppService } from './service/app.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent} from './login/login.component';
+import { XhrInterceptor } from './service/interceptor.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
